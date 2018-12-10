62547bbc6179ec54f458273b148d1328d802bd02
Andrew Norris <andrewmnorris@gmail.com>
2018-12-10 15:24:02 -0800

    version 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,9 @@ import "./Header.css";
 
 const Header = props => (
   <header className="header">
+  <div className="logo">
+  <img alt="logo" src="/assets/images/logo.png"></img>
+  </div>
     <h1>Clicky Game!</h1>
     <h2>
       Click on an image to earn points, but don't click on any more than once!
